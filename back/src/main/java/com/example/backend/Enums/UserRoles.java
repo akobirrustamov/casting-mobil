0ec565bde99c674fe5fdaf98407fc2f0b6534b9c
Akobir Rustamov <98689968+akobirrustamov@users.noreply.github.com>
2026-08-12 18:27:59 +0500

    delete backend

--- back/src/main/java/com/example/backend/Enums/UserRoles.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.Enums;
-
-public enum UserRoles {
-    ROLE_SUPERADMIN,
-    ROLE_ADMIN,
-    ROLE_USER,
-}
-
--- back/src/main/java/com/example/backend/Services/SecurityService/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.Services.SecurityService;
-
-import org.springframework.http.HttpEntity;
-
-public interface SecurityService {
-    HttpEntity<?> checkSecurity(String authorization);
-}
